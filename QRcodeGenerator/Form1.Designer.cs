7c5c6dfedb61879986552605657e34c74599b243
BStarcom <16Yoo>
2022-12-19 14:04:46 +0900

    QR Code Gen

---
+++ QRcodeGenerator/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace QRcodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PicBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.TBox_Input = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Btn_Save = new System.Windows.Forms.Button();
+            this.Btn_Gen = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PicBox
+            // 
+            this.PicBox.BackColor = System.Drawing.Color.White;
+            this.PicBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PicBox.Location = new System.Drawing.Point(3, 95);
+            this.PicBox.Name = "PicBox";
+            this.PicBox.Size = new System.Drawing.Size(378, 316);
+            this.PicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.PicBox.TabIndex = 0;
+            this.PicBox.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.PicBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TBox_Input, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(384, 461);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // TBox_Input
+            // 
+            this.TBox_Input.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TBox_Input.Location = new System.Drawing.Point(3, 3);
+            this.TBox_Input.Multiline = true;
+            this.TBox_Input.Name = "TBox_Input";
+            this.TBox_Input.Size = new System.Drawing.Size(378, 86);
+            this.TBox_Input.TabIndex = 2;
+            this.TBox_Input.Text = "\r\n\r\n\r\n\"URL\"";
+            this.TBox_Input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TBox_Input.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TBox_Input_MouseClick);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.Btn_Save, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Btn_Gen, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 417);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(378, 41);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // Btn_Save
+            // 
+            this.Btn_Save.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn_Save.Location = new System.Drawing.Point(192, 3);
+            this.Btn_Save.Name = "Btn_Save";
+            this.Btn_Save.Size = new System.Drawing.Size(183, 35);
+            this.Btn_Save.TabIndex = 1;
+            this.Btn_Save.Text = "Save";
+            this.Btn_Save.UseVisualStyleBackColor = true;
+            this.Btn_Save.Click += new System.EventHandler(this.Btn_Save_Click);
+            // 
+            // Btn_Gen
+            // 
+            this.Btn_Gen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn_Gen.Location = new System.Drawing.Point(3, 3);
+            this.Btn_Gen.Name = "Btn_Gen";
+            this.Btn_Gen.Size = new System.Drawing.Size(183, 35);
+            this.Btn_Gen.TabIndex = 0;
+            this.Btn_Gen.Text = "Generate";
+            this.Btn_Gen.UseVisualStyleBackColor = true;
+            this.Btn_Gen.Click += new System.EventHandler(this.Btn_Gen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "QR Code Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PicBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox TBox_Input;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button Btn_Save;
+        private System.Windows.Forms.Button Btn_Gen;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ QRcodeGenerator/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//
+using ZXing;
+
+namespace QRcodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        BarcodeWriter BarcodeWriter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+        //=======================================================================
+        // 초기화
+        //=======================================================================
+        public void Init()
+        {
+            BarcodeWriter = new BarcodeWriter();
+            BarcodeWriter.Format = BarcodeFormat.QR_CODE;
+        }
+        //=======================================================================
+        // URL => QR code 
+        //=======================================================================
+        public void MakeQr(string url)
+        {
+           PicBox.Image =  BarcodeWriter.Write(url);
+        }
+        //=======================================================================
+        // URL Input Box 최초 클릭시 내용 지워지도록
+        //=======================================================================
+        private void TBox_Input_MouseClick(object sender, MouseEventArgs e)
+        {
+            string tet = TBox_Input.Text.Trim().ToUpper();
+            if (TBox_Input.Text.Trim().ToUpper() == "\"URL\"")
+            {
+                TBox_Input.Text = string.Empty;
+            }
+        }
+        //=======================================================================
+        // QR Code 생성 버튼 클릭
+        //=======================================================================
+        private void Btn_Gen_Click(object sender, EventArgs e)
+        {
+            if(TBox_Input.Text == string.Empty)
+            {
+                MessageBox.Show("Check URL");
+                return;
+            }
+
+            MakeQr(TBox_Input.Text.Trim());
+        }
+        //=======================================================================
+        // 파일 저장 버튼 클릭
+        //=======================================================================
+        private void Btn_Save_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "JPEG File(*.jpg)|*.jpg|PNG File(*.png)|*.png";
+            saveFileDialog1.DefaultExt = "JPG";
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog1.FileName;
+                MakeJpg(fileName);
+            }
+        }
+        //=======================================================================
+        // 형식에 따른 이미지 파일 저장
+        //=======================================================================
+        public void MakeJpg(string filename)
+        {
+            switch(filename.Substring(filename.Length - 3, 3).ToUpper())
+            {
+                case "JPG":
+                    PicBox.Image.Save(filename, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    MessageBox.Show("JPG 저장 완료");
+                    break;
+
+                case "PNG":
+                    PicBox.Image.Save(filename, System.Drawing.Imaging.ImageFormat.Png);
+                    MessageBox.Show("PNG 저장 완료");
+                    break;
+
+                default:
+                    MessageBox.Show("저장 형식을 확인하세요.");
+                    break;
+            }
+        }
+        //=======================================================================
+    }
+}
